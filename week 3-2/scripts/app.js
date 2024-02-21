3ce6078b3fa864f920e4cf89ff586201faa36037
Kingsly <kingsly.bude@gmail.com>
2024-02-21 18:30:23 -0500

    02-21

--- week 3-2/scripts/app.js
+++ week 3-2/scripts/app.js
@@ -33,22 +33,72 @@
 
     function DisplayAboutUsPage(){
         console.log("ABOUT US PAGE");
-        let aboutUsButton  = document.getElementById("Btn");
     }
 
     function DisplayContactPage(){
         console.log("CONTACT PAGE");
-        let aboutUsButton  = document.getElementById("Btn");
+        let fullName = document.querySelector("#full-name");
+        let contactNumber = document.querySelector("#contact-number");
+        let emailAddress = document.querySelector("#email-address");
+
+        let sendButton = document.getElementById("send-button");
+        let subscribeCheckbox = document.getElementById("subscribe-checkbox");
+
+        sendButton.addEventListener("click", function(){
+            if(subscribeCheckbox.checked){
+                let newContact = new Contact(fullName.value, contactNumber.value, emailAddress.value);
+                if(newContact.serialize()){
+                    console.log(newContact.FullName);
+                    let key = newContact.FullName.substring(0,1) + Date.now();
+                    localStorage.setItem(key, newContact.serialize());
+                }
+            }
+        })
+    }
+
+    function DisplayContactListPage() {
+        console.log("CONTACT LIST PAGE");
+        if (localStorage.length > 0) {
+            let contactList = document.querySelector("#contact-list");
+
+            let data = "";
+            let keys = Object.keys(localStorage);
+
+            let index = 1;
+            for (const key of keys) {
+                let contactData = localStorage.getItem(key);
+                let contact = new Contact();
+
+                contact.deserialize(contactData);
+                data += `<tr><th scope="row" class="text-center">${index}</th>
+                        <td>${contact.FullName}</td>
+                        <td>${contact.ContactNumber}</td>
+                        <td>${contact.EmailAddress}</td>
+                        
+                        <td class="text-center">
+                            <button value="${key}" class="btn btn-primary btn-sm edit">
+                                <i class="fas fa-edit fa-sm"> edit</i>
+                            </button>
+                        </td>
+                        <td class="text-center">
+                            <button value="${key}" class="btn btn-danger btn-sm delete">
+                                <i class="fas fa-edit fa-sm"> delete</i>
+                            </button>
+                        </td>
+                        </tr>`;
+                index++;
+            }
+            storageKeys.foreach()
+
+        }
     }
 
     function DisplayProductsPage(){
         console.log("PRODUCTS PAGE");
-        let aboutUsButton  = document.getElementById("Btn");
     }
 
     function DisplayServicesPage(){
         console.log("SERVICES PAGE");
-        let aboutUsButton  = document.getElementById("Btn");
     }
 
     function Start(){
@@ -64,6 +114,9 @@
             case "Contact":
                 DisplayContactPage();
                 break;
+            case "Contact List":
+                DisplayContactListPage();
+                break;
             case "Products":
                 DisplayProductsPage();
                 break;
@@ -71,7 +124,11 @@
                 DisplayServicesPage();
                 break;
 
+
         }
     }
+
+
+
     window.addEventListener("load", Start)
 })();
