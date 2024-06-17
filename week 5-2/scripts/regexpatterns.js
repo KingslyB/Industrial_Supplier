6254c27fcf8ac2f960aa7747c2b78f7c3b245f72
Kingsly <kingsly.bude@gmail.com>
2024-06-17 18:44:17 -0400

    Reset

--- week 5-2/scripts/regexpatterns.js
+++
@@ -1,6 +0,0 @@
-// let fullNamePattern = /^([A-Z][a-z]{1,3}\.?\s)?([A-Z][a-z]+)+([\s,-]([A-z][a-z]+))*$/;
-// let contactNumberPattern =  /^(\+\d{1,3}[\s-.])?\(?\d{3}\)?[\s-.]?\d{3}[\s-.]\d{4}$/;
-// let emailAddressPattern =  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,10}$/;
-//
-// export {fullNamePattern, contactNumberPattern, emailAddressPattern};
-
